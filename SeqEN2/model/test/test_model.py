138b4fd0e15f2aa1c8e118332c77bbd015e88b3b
Hadi Nayebi <hadinayebi@gmail.com>
2022-01-17 21:28:02 -0500

    starting unittest model

---
+++ SeqEN2/model/test/test_model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Unit test Model."""
+
+from unittest import TestCase
+from unittest import main as unittest_main
+
+from SeqEN2.model.model import Model
+from SeqEN2.sessions.train_session import TrainSession
+
+
+class TestModel(TestCase):
+    """Test items for Model class"""
+
+    pass
+
+
+if __name__ == "__main__":
+    unittest_main()
