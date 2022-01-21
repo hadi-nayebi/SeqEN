c42b882569847ea111bf962ed116b660c6707072
Hadi Nayebi <hadinayebi@gmail.com>
2022-01-21 00:54:08 -0500

    loading multiple datasets add AAECSS customized train methods

--- SeqEN2/model/model.py
+++ SeqEN2/model/model.py
@@ -18,6 +18,9 @@ from SeqEN2.autoencoder.adversarial_autoencoder import AdversarialAutoencoder
 from SeqEN2.autoencoder.adversarial_autoencoder_classifier import (
     AdversarialAutoencoderClassifier,
 )
+from SeqEN2.autoencoder.adversarial_autoencoder_classifier_ss_decoder import (
+    AdversarialAutoencoderClassifierSSDecoder,
+)
 from SeqEN2.autoencoder.autoencoder import Autoencoder
 from SeqEN2.model.data_loader import DataLoader, write_json
 
@@ -39,8 +42,12 @@ class Model:
         self.device = device("cuda" if cuda.is_available() else "cpu")
         self.autoencoder = None
         self.build_model(arch)
-        self.data_loader = None
-        self.dataset_name = None
+        self.data_loader_cl = None
+        self.data_loader_ss = None
+        self.data_loader_clss = None
+        self.dataset_name_cl = None
+        self.dataset_name_ss = None
+        self.dataset_name_clss = None
         self.config = None
         if not self.path.exists():
             self.path.mkdir()
@@ -53,20 +60,80 @@ class Model:
             self.autoencoder = AdversarialAutoencoder(self.d1, self.dn, self.w, arch)
         elif arch.type == "AAEC":
             self.autoencoder = AdversarialAutoencoderClassifier(self.d1, self.dn, self.w, arch)
+        elif arch.type == "AAECSS":
+            self.autoencoder = AdversarialAutoencoderClassifierSSDecoder(
+                self.d1, self.dn, self.w, arch
+            )
         self.autoencoder.to(self.device)
 
-    def load_data(self, dataset_name):
+    def load_data(self, key, dataset_name):
         """
         Loading data once for a model to make sure the training/test sets are fixed.
+        :param key:
         :param dataset_name:
         :return:
         """
         # load datafiles
-        self.data_loader = DataLoader()
-        self.data_loader.load_test_data(dataset_name, self.device)
-        self.data_loader.load_train_data(dataset_name, self.device)
+        assert key in ["cl", "ss", "clss"], "unknown key for dataset"
+        if key == "cl":
+            self.data_loader_cl = DataLoader()
+            self.data_loader_cl.load_test_data(dataset_name, self.device)
+            self.data_loader_cl.load_train_data(dataset_name, self.device)
+            self.dataset_name_cl = dataset_name
+        elif key == "ss":
+            self.data_loader_ss = DataLoader()
+            self.data_loader_ss.load_test_data(dataset_name, self.device)
+            self.data_loader_ss.load_train_data(dataset_name, self.device)
+            self.dataset_name_ss = dataset_name
+        elif key == "clss":
+            self.data_loader_clss = DataLoader()
+            self.data_loader_clss.load_test_data(dataset_name, self.device)
+            self.data_loader_clss.load_train_data(dataset_name, self.device)
+            self.dataset_name_clss = dataset_name
 
     def train(
+        self,
+        epochs=10,
+        batch_size=128,
+        test_interval=100,
+        training_params=None,
+        input_noise=0.0,
+        log_every=100,
+        is_testing=False,
+    ):
+        """
+        The main training loop for a model
+        :param epochs:
+        :param batch_size:
+        :param test_interval:
+        :param training_params:
+        :param input_noise:
+        :param log_every:
+        :param is_testing:
+        :return:
+        """
+        if self.autoencoder.arch.type in ["AE", "AAE", "AAEC"]:
+            self.train_AAEC(
+                epochs=epochs,
+                batch_size=batch_size,
+                test_interval=test_interval,
+                training_params=training_params,
+                input_noise=input_noise,
+                log_every=log_every,
+                is_testing=is_testing,
+            )
+        elif self.autoencoder.arch.type == "AAECSS":
+            self.train_AAECSS(
+                epochs=epochs,
+                batch_size=batch_size,
+                test_interval=test_interval,
+                training_params=training_params,
+                input_noise=input_noise,
+                log_every=log_every,
+                is_testing=is_testing,
+            )
+
+    def train_AAEC(
         self,
         epochs=10,
         batch_size=128,
@@ -101,7 +168,7 @@ class Model:
         self.config = wandb.config
         self.config.batch_size = batch_size
         self.config.input_noise = input_noise
-        self.config.dataset_name = self.dataset_name
+        self.config.dataset_name_cl = self.dataset_name_cl
         self.autoencoder.initialize_for_training(training_params)
         self.config.training_params = self.autoencoder.training_params
         self.config.model_type = model_type
@@ -112,13 +179,13 @@ class Model:
         iter_for_test = 0
         iter_for_log = 0
         for epoch in range(0, epochs):
-            for batch in self.data_loader.get_train_batch(batch_size=batch_size):
+            for batch in self.data_loader_cl.get_train_batch(batch_size=batch_size):
                 self.autoencoder.train_batch(batch, self.device, input_noise=input_noise)
                 iter_for_test += 1
                 iter_for_log += 1
                 if iter_for_test == test_interval:
                     iter_for_test = 0
-                    self.test()
+                    self.test_AAEC()
                 if (iter_for_log + 1) % log_every == 0:
                     wandb.log({"epoch": epoch, "iter": iter_for_log})
                     for key, item in self.autoencoder.logs.items():
@@ -137,14 +204,106 @@ class Model:
         else:
             system(f"mv {str(train_dir)} {str(train_dir)}_done")
 
-    def test(self, num_test_items=1):
+    def test_AAEC(self, num_test_items=1):
         """
         The main training loop for a model
         :param num_test_items:
         :return:
         """
-        for test_batch, metadata in self.data_loader.get_test_batch(batch_size=num_test_items):
+        for test_batch, metadata in self.data_loader_cl.get_test_batch(batch_size=num_test_items):
             # using metadata?
+            self.autoencoder.test_batch(test_batch, self.device)
+
+    def train_AAECSS(
+        self,
+        epochs=10,
+        batch_size=128,
+        test_interval=100,
+        training_params=None,
+        input_noise=0.0,
+        log_every=100,
+        is_testing=False,
+    ):
+        """
+        The main training loop for a model
+        :param epochs:
+        :param batch_size:
+        :param test_interval:
+        :param training_params:
+        :param input_noise:
+        :param log_every:
+        :param is_testing:
+        :return:
+        """
+        now = datetime.now().strftime("%Y%m%d%H%M")
+        model_type = self.autoencoder.arch.type
+        arch_name = self.autoencoder.arch.name
+        run_title = f"{now}_{model_type}_{arch_name}"
+        train_dir = self.versions_path / f"{run_title}"
+        assert (
+            not train_dir.exists()
+        ), "This directory already exist, choose a different title for the run!"
+        train_dir.mkdir()
+        # connect to wandb
+        wandb.init(project=self.name, name=run_title)
+        self.config = wandb.config
+        self.config.batch_size = batch_size
+        self.config.input_noise = input_noise
+        self.config.dataset_name_cl = self.dataset_name_cl
+        self.config.dataset_name_ss = self.dataset_name_ss
+        self.autoencoder.initialize_for_training(training_params)
+        self.config.training_params = self.autoencoder.training_params
+        self.config.model_type = model_type
+        self.config.arch = arch_name
+        wandb.watch(self.autoencoder)
+        model = wandb.Artifact(f"{self.name}_model", type="model")
+        # start training loop
+        iter_for_test = 0
+        iter_for_log = 0
+        # finding correct size for data loader
+        max_size = max(self.data_loader_cl.train_data_size, self.data_loader_ss.train_data_size)
+        for epoch in range(0, epochs):
+            for batch_cl, batch_ss in zip(
+                self.data_loader_cl.get_train_batch(batch_size=batch_size, max_size=max_size),
+                self.data_loader_ss.get_train_batch(batch_size=batch_size, max_size=max_size),
+            ):
+                batch = {"cl": batch_cl, "ss": batch_ss}
+                self.autoencoder.train_batch(batch, self.device, input_noise=input_noise)
+                iter_for_test += 1
+                iter_for_log += 1
+                if iter_for_test == test_interval:
+                    iter_for_test = 0
+                    self.test_AAECSS()
+                if (iter_for_log + 1) % log_every == 0:
+                    wandb.log({"epoch": epoch, "iter": iter_for_log})
+                    for key, item in self.autoencoder.logs.items():
+                        wandb.log({key: wandb.Histogram(item), "iter": iter_for_log})
+                    self.autoencoder.reset_log()
+            model_path = str(train_dir / f"epoch_{epoch}.model")
+            torch_save(self.autoencoder, model_path)
+            model.add_file(model_path)
+            self.autoencoder.save(train_dir, epoch)
+            write_json(
+                self.autoencoder.training_params,
+                str(train_dir / f"{run_title}_train_params.json"),
+            )
+        if is_testing:
+            system(f"rm -r {train_dir}")
+        else:
+            system(f"mv {str(train_dir)} {str(train_dir)}_done")
+
+    def test_AAECSS(self, num_test_items=1):
+        """
+        The main training loop for a model
+        :param num_test_items:
+        :return:
+        """
+        for (batch_cl, metadata_cl), (batch_ss, metadata_ss) in zip(
+            self.data_loader_cl.get_test_batch(batch_size=num_test_items),
+            self.data_loader_ss.get_test_batch(batch_size=num_test_items),
+        ):
+            # using metadata?
+            test_batch = {"cl": batch_cl, "ss": batch_ss}
             self.autoencoder.test_batch(test_batch, self.device)
 
     def overfit(self, epochs=1000, num_test_items=1, input_noise=0.0, training_params=None):
