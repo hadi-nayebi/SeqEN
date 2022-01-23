0bf7fb9d3aa635b3a6acb49a5e802e81fb6016a0
Hadi Nayebi <hadinayebi@gmail.com>
2022-01-23 16:08:40 -0500

    adding test session

--- SeqEN2/utils/custom_arg_parser.py
+++ SeqEN2/utils/custom_arg_parser.py
@@ -87,3 +87,31 @@ class TrainSessionArgParser(DefaultParser):
             "-v", "--verbose", help="Verbose", action="store_true", default=False
         )
         self.parser.add_argument("-le", "--log_every", type=int, help="Log every", default=100)
+
+
+class TestSessionArgParser(DefaultParser):
+    """Set description, options, and flags for TestSession."""
+
+    def __init__(self) -> None:
+        """Define instance variables, collect arguments."""
+        super().__init__("Test a protein sequence autoencoder")
+
+    def _initialize(self) -> None:
+        self.parser.add_argument("-n", "--model", type=str, help="Model Name", required=True)
+        # dataset0: seq:act_p, dataset1: seq:ss, dataset2: seq:act_p:ss
+        self.parser.add_argument("-dcl", "--dataset_cl", type=str, help="Dataset_cl", required=True)
+        self.parser.add_argument("-dss", "--dataset_ss", type=str, help="Dataset_ss", default="")
+        self.parser.add_argument(
+            "-dclss", "--dataset_clss", type=str, help="Dataset_clss", default=""
+        )
+        # architecture blueprint
+        self.parser.add_argument("-a", "--arch", type=str, help="Arch", required=True)
+        # madel hyper params, d1: amino acids vec, dn: embedding, w: sliding window
+        self.parser.add_argument("-d1", "--d1", type=int, help="D1", default=8)
+        self.parser.add_argument("-dn", "--dn", type=int, help="Dn", default=10)
+        self.parser.add_argument("-w", "--w", type=int, help="W", default=20)
+        self.parser.add_argument("-teb", "--test_batch", type=int, help="Test Batch", default=1)
+        self.parser.add_argument("-mid", "--model_id", type=str, help="Model ID", default="")
+        self.parser.add_argument(
+            "-v", "--verbose", help="Verbose", action="store_true", default=False
+        )
