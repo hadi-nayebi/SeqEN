e6bc333e9a779a25f445e162106d6f1f17eee01b
Hadi Nayebi <hadinayebi@gmail.com>
2022-01-23 20:52:44 -0500

    adding args for embedding, tsne, version, model_id

--- SeqEN2/utils/custom_arg_parser.py
+++ SeqEN2/utils/custom_arg_parser.py
@@ -98,6 +98,10 @@ class TestSessionArgParser(DefaultParser):
 
     def _initialize(self) -> None:
         self.parser.add_argument("-n", "--model", type=str, help="Model Name", required=True)
+        self.parser.add_argument(
+            "-mv", "--model_version", type=str, help="Model Version", required=True
+        )
+        self.parser.add_argument("-mid", "--model_id", type=int, help="Model ID", required=True)
         # dataset0: seq:act_p, dataset1: seq:ss, dataset2: seq:act_p:ss
         self.parser.add_argument("-dcl", "--dataset_cl", type=str, help="Dataset_cl", required=True)
         self.parser.add_argument("-dss", "--dataset_ss", type=str, help="Dataset_ss", default="")
@@ -111,7 +115,10 @@ class TestSessionArgParser(DefaultParser):
         self.parser.add_argument("-dn", "--dn", type=int, help="Dn", default=10)
         self.parser.add_argument("-w", "--w", type=int, help="W", default=20)
         self.parser.add_argument("-teb", "--test_batch", type=int, help="Test Batch", default=1)
-        self.parser.add_argument("-mid", "--model_id", type=str, help="Model ID", default="")
         self.parser.add_argument(
             "-v", "--verbose", help="Verbose", action="store_true", default=False
         )
+        self.parser.add_argument(
+            "-ge", "--get_embedding", help="Get Embedding", action="store_true", default=False
+        )
+        self.parser.add_argument("-tsne", "--tsne_dim", type=int, help="tSNE dim", default=0)
