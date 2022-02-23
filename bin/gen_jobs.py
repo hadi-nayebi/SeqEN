866677a8a929bac423f822f973367b163a0b749a
hadinayebi <hadinayebi@gmail.com>
2022-02-23 15:35:13 -0500

    update job params

--- bin/gen_jobs.py
+++ bin/gen_jobs.py
@@ -10,6 +10,8 @@ pass_dataset_cl = argv[4]  # 0 or 1
 pass_dataset_ss = argv[5]  # 0 or 1
 pass_dataset_clss = argv[6]  # 0 or 1
 arch = f"-a {argv[7]}"  # arch20
+epoch = int(argv[8])
+trb = int(argv[9])
 
 
 noise = "n" if train_with_noise == "1" else "0"
@@ -23,15 +25,15 @@ dataset_ss_arg = "-dss pdb_ndx_ss" if pass_dataset_ss == "1" else ""
 dataset_clss_arg = "-dclss pdb_act_clss" if pass_dataset_clss == "1" else ""
 
 noise_arg = "-no 0.05" if train_with_noise == "1" else ""
-
-
+epoch_arg = f"-e {epoch}"
+trb_arg = f"-trb {trb}"
 system(f"mkdir exp{job_id}_jobs")
 
 
-for mid_val in range(0, 1000, 5):
+for mid_val in range(0, 1000, epoch):
     mid = mid_val if mid_val == 0 else mid_val - 1
-    mvid_arg = f"-mvid VERSION#{mid}" if mid > 3 else ""
-    next_job = mid + 5 if mid_val != 0 else mid + 4
+    mvid_arg = f"-mvid VERSION#{mid}" if mid > 0 else ""
+    next_job = mid + epoch if mid_val != 0 else mid + epoch - 1
     job_script_template = f"""#!/bin/bash --login
 ########## SBATCH Lines for Resource Request ##########
  
@@ -48,7 +50,7 @@ conda activate SeqEN
 cd /mnt/home/nayebiga/SeqEncoder/SeqEN/
 module load GCCcore/11.1.0 Python/3.9.6
 module load GCC/11.1.0-cuda-11.4.2  CUDA/11.4.2
-python3 ./SeqEN2/sessions/train_session.py -n experiment{job_id} {dataset_cl_arg} {dataset_ss_arg} {dataset_clss_arg} {arch} {noise_arg} {nc} -trb 10 -w 20 -d1 8 -ts params4 -ti 100 -le 1000 -e 5 {mvid_arg}
+python3 ./SeqEN2/sessions/train_session.py -n experiment{job_id} {dataset_cl_arg} {dataset_ss_arg} {dataset_clss_arg} {arch} {noise_arg} {nc} {trb_arg} -w 20 -d1 8 -ts params4 -ti 100 -le 1000 {epoch} {mvid_arg}
 
 scontrol show job $SLURM_JOB_ID                                       ### write job information to SLURM output file.
 js -j $SLURM_JOB_ID                                                   ### write resource usage to SLURM output file (powertools command).
