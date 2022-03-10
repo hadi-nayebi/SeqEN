cedc512fa4e6350bec48b7afe3458c5466608b18
hadinayebi <hadinayebi@gmail.com>
2022-03-10 17:24:52 -0500

    add embed_one_batch

--- SeqEN2/autoencoder/autoencoder_classifier_ss_decoder.py
+++ SeqEN2/autoencoder/autoencoder_classifier_ss_decoder.py
@@ -122,3 +122,23 @@ class AutoencoderClassifierSSDecoder(AutoencoderClassifier, AutoencoderSSDecoder
                 self.test_one_batch(input_vals["ss"], device, input_keys="AS")
             if "clss" in input_vals.keys():
                 self.test_one_batch(input_vals["clss"], device, input_keys="ASC")
+
+    def eval_one_batch(self, input_vals, device, input_keys="A--", embed_only=False):
+        if input_vals is not None:
+            _, _, _, one_hot_input = self.transform_input(input_vals, device, input_keys=input_keys)
+            if embed_only:
+                encoded_output = self.forward_embed(one_hot_input)
+                return {"embedding": encoded_output}
+            else:
+                (
+                    reconstructor_output,
+                    classifier_output,
+                    ss_decoder_output,
+                    encoded_output,
+                ) = self.forward_test(one_hot_input)
+                return {
+                    "reconstructor_output": reconstructor_output,
+                    "classifier_output": classifier_output,
+                    "ss_decoder_output": ss_decoder_output,
+                    "embedding": encoded_output,
+                }
