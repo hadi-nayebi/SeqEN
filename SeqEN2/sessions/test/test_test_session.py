4c4e4832d1bff01ff7fe9e5f457f55d3928c7110
hadinayebi <hadinayebi@gmail.com>
2022-03-11 02:33:01 -0500

    test embeddings

--- SeqEN2/sessions/test/test_test_session.py
+++ SeqEN2/sessions/test/test_test_session.py
@@ -12,46 +12,47 @@ from torch.nn import Sequential
 from SeqEN2.sessions.test_session import TestSession
 
 
-class TestTrainSession(TestCase):
+class TestTestSession(TestCase):
     """Test items for TestSession class"""
 
     test_session = TestSession()
-    DATASET_NAME_seq_ACTp = "kegg_ndx_ACTp_100"
-    DATASET_NAME_seq_ss = "pdb_ndx_ss_100"
-    MODEL_NAME = "dummy"
-    ARCH = "arch6"
-    VERSION = "test_AAECSS_arch6"
-    MODEL_ID = 0
+    DATASET_NAME = "pdb_act_clss_train"
+    MODEL_NAME = "experiment0125"
+    ARCH = "arch7"
+    VERSION = "202201261847_AAECSS_arch7"
+    MODEL_ID = 300
+    TEST_ITEMS = 10
 
     @classmethod
     def setUpClass(cls) -> None:
         cls.test_session.add_model(
             cls.MODEL_NAME, cls.ARCH, cls.VERSION, cls.MODEL_ID, d1=8, dn=10, w=20
         )
-        cls.test_session.load_data("cl", cls.DATASET_NAME_seq_ACTp)
-        cls.test_session.get_embedding(num_test_items=10)
+        cls.test_session.load_data("clss", cls.DATASET_NAME)
+        cls.test_session.get_embedding(num_test_items=cls.TEST_ITEMS)
 
     def test_add_model(self):
         self.assertIsInstance(self.test_session.model.autoencoder.vectorizer, Sequential)
+        self.assertIsInstance(self.test_session.model.autoencoder.devectorizer, Sequential)
         self.assertIsInstance(self.test_session.model.autoencoder.encoder, Sequential)
         self.assertIsInstance(self.test_session.model.autoencoder.decoder, Sequential)
-        self.assertIsInstance(self.test_session.model.autoencoder.ss_decoder, Sequential)
-        self.assertIsInstance(self.test_session.model.autoencoder.classifier, Sequential)
-        self.assertIsInstance(self.test_session.model.autoencoder.discriminator, Sequential)
+        # self.assertIsInstance(self.test_session.model.autoencoder.ss_decoder, Sequential)
+        # self.assertIsInstance(self.test_session.model.autoencoder.classifier, Sequential)
+        # self.assertIsInstance(self.test_session.model.autoencoder.discriminator, Sequential)
 
     def test_get_embedding(self):
-        self.assertEqual(10, len(self.test_session.embedding_results))
+        if self.TEST_ITEMS != -1:
+            self.assertEqual(self.TEST_ITEMS, len(self.test_session.embedding_results))
         one_embedding = list(self.test_session.embedding_results.values())[0]
         self.assertIsInstance(self.test_session.embedding_results, Dict)
         self.assertIsInstance(one_embedding, DataFrame)
-        self.assertIn("act_pred", one_embedding.columns, "missing a column un results")
-        self.assertIn("act_trg", one_embedding.columns, "missing a column un results")
-        self.assertIn("slices", one_embedding.columns, "missing a column un results")
-        self.assertIn("embedding", one_embedding.columns, "missing a column un results")
+        # print(one_embedding.attrs)
+        # print(one_embedding.columns)
+        # print(one_embedding)
 
     def test_tsne_embeddings(self):
-        pass
-        # self.test_session.plot_embedding_2d()
+        # pass
+        self.test_session.plot_embedding_2d(auto_open=True)
         # opens two html files
 
 
