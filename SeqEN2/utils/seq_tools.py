9487882e72a979ad15a07984431f00df1f34430f
hadinayebi <hadinayebi@gmail.com>
2022-03-10 19:08:05 -0500

    add specific methods

--- SeqEN2/utils/seq_tools.py
+++ SeqEN2/utils/seq_tools.py
@@ -4,17 +4,21 @@
 # by nayebiga@msu.edu
 __version__ = "0.0.1"
 
+from numpy.random import choice
 from torch import (
     Tensor,
     argmax,
+    cat,
     diagonal,
     empty,
     eye,
     fliplr,
     index_select,
     mode,
-    tensor,
+    randperm,
 )
+from torch import sum as torch_sum
+from torch import tensor
 from torch.nn.functional import unfold
 
 
@@ -51,17 +55,16 @@ def get_seq(ndx, ndx_windows):
 
 
 def get_consensus_seq(seq, device):
-    if len(seq.shape) == 3:
-        seq = argmax(seq, dim=1).reshape((-1, seq.shape[1]))
     output_length, w = seq.shape
     seq_length = output_length + w - 1
-    output = empty(seq_length, device=device)
+    consensus_seq = empty(seq_length, device=device)
     for i in range(seq_length):
-        output[i] = consensus(seq, i, device=device)
-    return output
+        consensus_seq[i] = consensus(seq, i, device=device)
+    return consensus_seq
 
 
-def consensus_acc(seq, output, device):
+def consensus_acc(seq, output, w, device):
+    output = output_to_ndx(output, w)
     output_length, w = output.shape
     seq_length = output_length + w - 1
     n = 0
@@ -90,3 +93,55 @@ def sliding_window(input_vals, w, keys=None):
 def ndx_to_seq(seq, keys):
     assert isinstance(seq, Tensor)
     return "".join([keys[i] for i in seq.long()])
+
+
+def output_to_ndx(output, w):
+    return argmax(output, dim=1).reshape((-1, w))
+
+
+def reconstructor_acc(output, input_ndx):
+    return torch_sum(argmax(output, dim=1) == input_ndx.reshape((-1,))) / output.shape[0]
+
+
+def add_noise(one_hot_input, input_noise, device):
+    ndx = randperm(one_hot_input.shape[1])
+    size = list(one_hot_input.shape)
+    size[-1] = 1
+    p = tensor(choice([1, 0], p=[input_noise, 1 - input_noise], size=size)).to(device)
+    return (one_hot_input[:, ndx, :] * p) + (one_hot_input * (1 - p))
+
+
+def slide_window(input_vals, w):
+    kernel_size = (input_vals.shape[1], w)
+    return unfold(input_vals.float().T[None, None, :, :], kernel_size=kernel_size)[0].T
+
+
+def split_input_vals(input_vals, input_keys, w):
+    target_vals_ss = None
+    target_vals_cl = None
+    if len(input_keys) == 2:
+        if input_keys[1] == "S":
+            target_vals_ss = input_vals[:, w:].long()
+        elif input_keys[1] == "C":
+            target_vals_cl = input_vals[:, w:].mean(axis=1).reshape((-1, 1))
+            target_vals_cl = cat((target_vals_cl, 1 - target_vals_cl), 1).float()
+    elif len(input_keys) == 3:
+        if input_keys[1] == "S":
+            target_vals_ss = input_vals[:, w:-w].long()
+        elif input_keys[2] == "S":
+            target_vals_ss = input_vals[:, -w:].long()
+        if input_keys[1] == "C":
+            target_vals_cl = input_vals[:, w:-w].mean(axis=1).reshape((-1, 1))
+            target_vals_cl = cat((target_vals_cl, 1 - target_vals_cl), 1).float()
+        elif input_keys[2] == "C":
+            target_vals_cl = input_vals[:, -w:].mean(axis=1).reshape((-1, 1))
+            target_vals_cl = cat((target_vals_cl, 1 - target_vals_cl), 1).float()
+    return target_vals_ss, target_vals_cl
+
+
+def continuity_target_right(encoded_output):
+    return cat((encoded_output[1:], encoded_output[-1].unsqueeze(0)), 0)
+
+
+def continuity_target_left(encoded_output):
+    return cat((encoded_output[0].unsqueeze(0), encoded_output[:-1]), 0)
