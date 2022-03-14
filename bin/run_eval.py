2fc024e84b5abf73d260b5b3d9c9dcb35dc87ca1
hadinayebi <hadinayebi@gmail.com>
2022-03-14 16:50:47 -0400

    kegg embeddings

--- bin/run_eval.py
+++ bin/run_eval.py
@@ -2,16 +2,16 @@ from os import system
 
 models = {
     "AECSS": [
-        ("202203042153_AECSS_arch66", 24, "arch66"),
-        ("202203050040_AECSS_arch69", 24, "arch69"),
+        # ("202203042153_AECSS_arch66", 24, "arch66"),
+        # ("202203050040_AECSS_arch69", 24, "arch69"),
         ("202203050123_AECSS_arch70", 45, "arch70"),
-        ("202203042153_AECSS_arch66_dcl", 29, "arch66"),
+        # ("202203042153_AECSS_arch66_dcl", 29, "arch66"),
     ]
 }
 for v, n, a in models["AECSS"]:
-    system(
-        f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n AECSS -mv {v} -mid {n} -dclss single_act_clss_test -a {a} -teb -1 -tsne 2 -prids Q8LJW3-F1-model_v1_1,2JOQ -t sample"
-    )
     # system(
-    #     f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n AECSS -mv {v} -mid {n} -dcl kegg_ndx_ACTp_test -a {a} -teb 1000 -ge -tsne 2"
+    #     f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n AECSS -mv {v} -mid {n} -dclss single_act_clss_test -a {a} -teb -1 -tsne 2 -prids Q8LJW3-F1-model_v1_1,2JOQ -t sample"
     # )
+    system(
+        f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n AECSS -mv {v} -mid {n} -dcl kegg_ndx_ACTp_test -a {a} -teb -1 -ge"
+    )
