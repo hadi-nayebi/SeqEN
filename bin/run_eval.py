1e6c40e6fb73dc4e3fd193cabbe627034eb4af76
hadinayebi <hadinayebi@gmail.com>
2022-03-25 06:50:25 -0400

    update run eval

--- bin/run_eval.py
+++ bin/run_eval.py
@@ -10,7 +10,7 @@ models = {
 }
 for v, n, a in models["AECSS"]:
     system(
-        f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n LONG -mv {v} -mid {n} -dclss single_act_clss -a {a} -teb -1 -tsne 2 -prids Q8LJW3-F1-model_v1_1,2JOQ"
+        f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n LONG -mv {v} -mid {n} -dclss single_act_clss -a {a} -teb -1 -tsne 2 -prids Q8LJW3-F1-model_v1_1,2JOQ -ge"
     )
     # system(
     #     f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n AECSS -mv {v} -mid {n} -dcl kegg_ndx_ACTp_test -a {a} -teb -1 -ge"
