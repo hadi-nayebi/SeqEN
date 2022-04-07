ef73db7ab7d5d9e45dbad659a4fcb662450193dc
hadinayebi <hadinayebi@gmail.com>
2022-04-06 21:04:32 -0400

    update run eval

--- bin/run_eval.py
+++ bin/run_eval.py
@@ -4,7 +4,7 @@ models = {
     "AECSS": [
         # ("202203042153_AECSS_arch66", 24, "arch66"),
         # ("202203050040_AECSS_arch69", 24, "arch69"),
-        ("202203050123_AECSS_arch70", 548, "arch70"),
+        ("202203050123_AECSS_arch70", 544, "arch70"),
         # ("202203042153_AECSS_arch66_dcl", 29, "arch66"),
     ]
 }
