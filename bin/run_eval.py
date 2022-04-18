d7ce29f58b7f36890ee118a84702a508327d51dc
hadinayebi <hadinayebi@gmail.com>
2022-04-18 14:44:06 -0400

    update run eval

--- bin/run_eval.py
+++ bin/run_eval.py
@@ -2,13 +2,20 @@ from os import system
 
 models = {
     "AECSS": [
-        ("202203050123_AECSS_arch70", 199, "arch70"),
-        ("202203050123_AECSS_arch70", 299, "arch70"),
-        ("202203050123_AECSS_arch70", 399, "arch70"),
-        ("202203050123_AECSS_arch70", 499, "arch70"),
-        ("202203050123_AECSS_arch70", 599, "arch70"),
-        ("202203050123_AECSS_arch70", 699, "arch70"),
-        ("202203050123_AECSS_arch70", 750, "arch70"),
+        # ("202203050123_AECSS_arch70", 199, "arch70"),
+        # ("202203050123_AECSS_arch70", 299, "arch70"),
+        # ("202203050123_AECSS_arch70", 399, "arch70"),
+        # ("202203050123_AECSS_arch70", 499, "arch70"),
+        # ("202203050123_AECSS_arch70", 599, "arch70"),
+        # ("202203050123_AECSS_arch70", 699, "arch70"),
+        # ("202203050123_AECSS_arch70", 750, "arch70"),
+        ("202203050123_AECSS_arch70", 799, "arch70"),
+        # ("202203050123_AECSS_arch70", 899, "arch70"),
+        # ("202203050123_AECSS_arch70", 999, "arch70"),
+        # ("202203050123_AECSS_arch70", 499, "arch70"),
+        # ("202203050123_AECSS_arch70", 599, "arch70"),
+        # ("202203050123_AECSS_arch70", 699, "arch70"),
+        # ("202203050123_AECSS_arch70", 750, "arch70"),
     ]
 }
 for v, n, a in models["AECSS"]:
