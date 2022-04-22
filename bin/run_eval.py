f8a30a9476f64cbf724a64b34a35b08bd9a19b51
hadinayebi <hadinayebi@gmail.com>
2022-04-22 01:09:11 -0400

    update run eval

--- bin/run_eval.py
+++ bin/run_eval.py
@@ -9,8 +9,8 @@ models = {
         # ("202203050123_AECSS_arch70", 599, "arch70"),
         # ("202203050123_AECSS_arch70", 699, "arch70"),
         # ("202203050123_AECSS_arch70", 750, "arch70"),
-        ("202203050123_AECSS_arch70", 799, "arch70"),
-        # ("202203050123_AECSS_arch70", 899, "arch70"),
+        # ("202203050123_AECSS_arch70", 799, "arch70"),
+        ("202203050123_AECSS_arch70", 854, "arch70"),
         # ("202203050123_AECSS_arch70", 999, "arch70"),
         # ("202203050123_AECSS_arch70", 499, "arch70"),
         # ("202203050123_AECSS_arch70", 599, "arch70"),
@@ -19,15 +19,15 @@ models = {
     ]
 }
 for v, n, a in models["AECSS"]:
-    # system(
-    #     f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n LONG -mv {v} -mid {n} -dclss clss -a {a} -teb -1 -ge"
-    # )
+    system(
+        f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n LONG -mv {v} -mid {n} -dclss clss -a {a} -teb -1 -ge"
+    )
     system(
         f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n LONG -mv {v} -mid {n} -dclss single_act_clss -a {a} -teb -1 -tsne 2 -ge"
     )
-    # system(
-    #     f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n LONG -mv {v} -mid {n} -dcl cl_test -a {a} -teb -1 -ge"
-    # )
+    system(
+        f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n LONG -mv {v} -mid {n} -dcl cl_test -a {a} -teb -1 -ge"
+    )
     # system(
     #     f"python3 ~/SeqEncoder/SeqEN/SeqEN2/sessions/test_session.py -n LONG -mv {v} -mid {n} -dcl cl_train -a {a} -teb -1 -ge"
     # )
